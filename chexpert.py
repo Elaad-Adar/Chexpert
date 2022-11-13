8e042df19cb42997be30e027854fe2e446344258
elaadadar <elaad.adar@checkmarx.com>
2022-11-13 13:49:59 +0200

    commented out attn visualize

--- chexpert.py
+++ chexpert.py
@@ -618,15 +618,15 @@ if __name__ == '__main__':
 
     if args.visualize:
         visualize(model, vis_dataloader, grad_cam_hooks, args)
-        if grad_cam_hooks is not None:
-            print(f'grad_cam_hooks.items - {grad_cam_hooks.items()}')
-            print(f'grad_cam_hooks.keys - {grad_cam_hooks.keys()}')
-            print(f'grad_cam_hooks.values - {grad_cam_hooks.values()}')
-            for x, _, idxs in vis_dataloader:
-                model(x.to(args.device))
-                patient_ids = extract_patient_ids(vis_dataloader.dataset, idxs)
-                # visualize stored attention weights for each image
-                for i in range(len(x)): vis_attn(x, patient_ids, idxs, grad_cam_hooks, args, i)
+        # if grad_cam_hooks is not None:
+        #     print(f'grad_cam_hooks.items - {grad_cam_hooks.items()}')
+        #     print(f'grad_cam_hooks.keys - {grad_cam_hooks.keys()}')
+        #     print(f'grad_cam_hooks.values - {grad_cam_hooks.values()}')
+        #     for x, _, idxs in vis_dataloader:
+        #         model(x.to(args.device))
+        #         patient_ids = extract_patient_ids(vis_dataloader.dataset, idxs)
+        #         # visualize stored attention weights for each image
+        #         for i in range(len(x)): vis_attn(x, patient_ids, idxs, grad_cam_hooks, args, i)
 
     if args.plot_roc:
         # load results files from output_dir
